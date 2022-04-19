f7654e16272e6ee3cad002a5c6299af8756051b1
Alexandre <lutosaalexandre@gmail.com>
2022-04-19 15:19:26 -0300

    feat: login input and password implemented

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+    <div class="mb-3">
+      <label for='name-input' class="form-label">Name</label>
+      <input id='name-input' type="email" class="form-control" placeholder="name@example.com" />
+    </div>
+    <div class="mb-3">
+      <label for='password-input' class="visually-hidden">Password</label>
+      <input type="password" class="form-control" id='password-input' placeholder="Password" />
+    </div>
+  </div>
+    )
 }
