f3fd640bdf54d936982cbbc0473cf487f5722585
Alexandre <lutosaalexandre@gmail.com>
2022-04-23 12:05:42 -0300

    feat: show favorited musics on favorite page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
+import SongCard from '../components/SongCard'
+import { getFromLocalStorage } from '../utils/localStorageHandler'
 
 export default function Favorites() {
+
+  const [favoriteSongs, setFavoriteSongs] = useState([])
+
+  useEffect(() => {
+    const localStorageFavoriteSongs = getFromLocalStorage('favoriteSongs')
+    setFavoriteSongs(localStorageFavoriteSongs)
+  }, [])
+  
+
   return (
     <div>
       <Header />
-      Favorites
+      {favoriteSongs.map(song => <SongCard song={song} />)}
     </div>
   )
 }
