2482fa77c237796b5cde0d902816eda0a82c37e1
Alexandre <lutosaalexandre@gmail.com>
2022-04-23 09:49:36 -0300

    feat: AlbumDetails page and SongCard component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Search from './pages/Search'
 import NotFound from './pages/NotFound'
 import Profile from './pages/Profile'
 import Album from './pages/Album'
+import AlbumDetails from './pages/AlbumDetails'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path={"/search"} component={Search}/>
         <Route exact path={'/profile'} component={Profile}/>
         <Route exact path={'/album'} component={Album}/>
+        <Route exact path={'/album/:id'} component={AlbumDetails}/>
         <Route exact path={'/profile/edit'} />
         <Route exact path={'*'} component={NotFound}/>
       </Switch>
